69a2fe0cbe28e05655986e8e752c91eb7df77041
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-09-07 08:22:39 +0600

    added CSS styling and most JS functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,34 @@
+//generates random id;
+let guid = () => {
+  let s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  return (
+    s4() +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    s4() +
+    s4()
+  );
+};
+
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = guid();
   this.info = () =>
     `${title} by ${author}, ${pages} ${pages > 1 ? "pages" : "page"}, ${
       read ? "read" : "not read yet"
@@ -10,5 +36,108 @@ function Book(title, author, pages, read) {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const HarryPotter = new Book("Harry Potter", "J. K Rowling", 321, true);
+const Sapiens = new Book("Sapines", "Yuval Noah Harari", 431, false);
 
-console.log(theHobbit.info());
+myLibrary.push(theHobbit);
+myLibrary.push(HarryPotter);
+myLibrary.push(Sapiens);
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = event.target;
+
+  addBookToLibrary({
+    title: input.title.value,
+    author: input.author.value,
+    pages: input.pages.value,
+    readStatus: input.readStatus.value,
+  });
+
+  input.title.value = "";
+  input.author.value = "";
+  input.pages.value = "";
+  input.readStatus.value = "";
+});
+
+function addBookToLibrary(userInput) {
+  const newBook = new Book(
+    userInput.title,
+    userInput.author,
+    Number(userInput.pages),
+    userInput.readStatus === "read" ? true : false
+  );
+  myLibrary.push(newBook);
+  displayLibrary();
+}
+
+function removeBook(id) {
+  myLibrary = myLibrary.filter((book) => book.id !== id);
+  displayLibrary();
+}
+
+function displayLibrary() {
+  const displayDiv = document.getElementById("bookDisplay");
+
+  displayDiv.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    const outerDiv = document.createElement("div");
+    outerDiv.classList.add(
+      "mr-auto",
+      "ml-auto",
+      "text-center",
+      "mt-3",
+      "col-4"
+    );
+
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("card", "align-items-center");
+    innerDiv.style.height = "250px";
+    innerDiv.style.flexDirection = "row";
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.textContent = book.title;
+
+    const author = document.createElement("p");
+    author.classList.add("card-text");
+    const authorName = document.createElement("em");
+    authorName.textContent = book.author;
+    author.appendChild(authorName);
+
+    const page = document.createElement("p");
+    page.classList.add("card-text");
+    const pageNumber = document.createElement("strong");
+    pageNumber.textContent = `${book.pages} pages`;
+    page.appendChild(pageNumber);
+
+    const readStatus = document.createElement("a");
+    readStatus.classList.add(
+      "btn",
+      book.read === true ? "btn-success" : "btn-warning",
+      "m-1"
+    );
+    readStatus.href = "#";
+    readStatus.textContent = book.read === true ? "Read" : "Not read yet";
+
+    const deleteButton = document.createElement("a");
+    deleteButton.classList.add("btn", "btn-danger", "m-1");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => removeBook(book.id));
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(author);
+    cardBody.appendChild(page);
+    cardBody.appendChild(readStatus);
+    cardBody.appendChild(deleteButton);
+    innerDiv.appendChild(cardBody);
+    outerDiv.appendChild(innerDiv);
+    displayDiv.appendChild(outerDiv);
+  });
+}
+
+displayLibrary();
